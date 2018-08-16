9d699d682f02d3e0a9fd813da799d92981db0dbc
Myqilixiang <1141421776@qq.com>
2018-08-16 18:30:07 +0800

    添加mockjsu 用来模拟后台数据

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import router from './router'
 import store from './store'
+import './mock' // simulation data
 
 import '@/icons' // icon
 import '@/permission' // permission control
--- src/mock/index.js
+++ src/mock/index.js
@@ -6,8 +6,8 @@ import loginAPI from './login'
 // })
 
 // 登录相关
-Mock.mock(/\/login\/login/, 'post', loginAPI.loginByUsername)
-Mock.mock(/\/login\/logout/, 'post', loginAPI.logout)
+Mock.mock(/\/user\/login/, 'post', loginAPI.loginByUsername)
+Mock.mock(/\/user\/logout/, 'post', loginAPI.logout)
 Mock.mock(/\/user\/info\.*/, 'get', loginAPI.getUserInfo)
 
 export default Mock
